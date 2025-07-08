95e5c4dbf29727f4df4a8ce3d2e2f59218b192e5
eatsky1006 <eatsky1006@gmail.com>
2025-07-08 19:14:24 -0300

    ping-viewer-next-frontend: minor linter fix

--- ping-viewer-next-frontend/src/components/widgets/sonar1d/WaterfallShader.vue
+++ ping-viewer-next-frontend/src/components/widgets/sonar1d/WaterfallShader.vue
@@ -203,7 +203,7 @@ export default {
       if (redrawAll) {
         textureData.fill(0);
 
-        measurementHistory.value.forEach((measurement, columnIndex) => {
+        for (const [columnIndex, measurement] of measurementHistory.value.entries()) {
           if (columnIndex >= newWidth) return;
 
           const x = newWidth - 1 - columnIndex;
@@ -238,7 +238,7 @@ export default {
               textureData[index + 3] = color[3] !== undefined ? color[3] : 255;
             }
           }
-        });
+        }
       } else {
         // Just shift existing data
         for (let y = 0; y < newHeight; y++) {
