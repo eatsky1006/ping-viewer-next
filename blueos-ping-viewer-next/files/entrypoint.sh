0c24d175a6f84d56d369034faa3af643b650ac3b
eatsky1006 <eatsky1006@gmail.com>
2025-07-09 13:56:51 -0300

    blueos-extension: Dockerfile: Fix recordings folder permission

--- blueos-ping-viewer-next/files/entrypoint.sh
+++ blueos-ping-viewer-next/files/entrypoint.sh
@@ -3,6 +3,9 @@ set -m
 echo "Starting ping viewer next..."
 cd app
 mkdir logs
+mkdir recordings
 chmod -R 755 /app/logs
+chmod -R 755 /app/recordings
 chown -R pingviewer:pingviewer /app/logs
+chown -R pingviewer:pingviewer /app/recordings
 su pingviewer -c "./ping-viewer-next --enable-auto-create --rest-server 0.0.0.0:6060"
